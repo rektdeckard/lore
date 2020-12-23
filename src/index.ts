6168a5502b27c014d0badda796af9eebca8bf441
rektdeckard <friedtm@gmail.com>
2020-12-23 02:37:30 -0500

    Bot: install db dor server-specific settings

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import { CommandoClient } from "discord.js-commando";
-// import path from "path";
+import { CommandoClient, SQLiteProvider } from "discord.js-commando";
+import path from "path";
+import * as sqlite from "sqlite";
 import * as dotenv from "dotenv";
 import * as API from "./api";
 
 const bot = new CommandoClient({
   commandPrefix: "!",
   owner: "337415396330045440",
+  nonCommandEditable: true,
   // invite: 'https://discord.gg/bRCvFy9',
 });
 
@@ -19,8 +21,19 @@ bot.registry
     ["sessions", "List or show details for sessions"],
   ])
   .registerDefaultGroups()
-  .registerDefaultCommands()
-  // .registerCommandsIn(path.join(__dirname, "commands"));
+  .registerDefaultCommands({ help: false, unknownCommand: false });
+// .registerCommandsIn(path.join(__dirname, "commands"));
+
+bot
+  .setProvider(
+    sqlite
+      .open({
+        filename: path.join(__dirname, "settings.sqlite3"),
+        driver: sqlite.Database,
+      })
+      .then((db) => new SQLiteProvider(db))
+  )
+  .catch(console.error);
 
 bot.on("ready", () => {
   console.info(`Logged in as ${bot.user?.tag ?? "Lore"}!`);
